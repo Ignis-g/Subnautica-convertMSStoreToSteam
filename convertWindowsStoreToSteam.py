b9a601639f8ee1fa3625e37887c0c6046f0063f7
Ignis-g <105871593+Ignis-g@users.noreply.github.com>
2022-05-19 17:20:46 +0200

    Initial commit.

---
+++ convertWindowsStoreToSteam.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+
+import re
+from os import mkdir, path
+from zlib import decompress
+from struct import unpack
+from datetime import date
+from time import time
+from glob import glob
+
+savePath = './windowsStoreSave/'
+
+def retrieveMd5FileName(content):
+	return (content[0:4][::-1].hex() + \
+			content[4:6][::-1].hex() + \
+			content[6:8][::-1].hex() + \
+			content[8:16].hex()).upper()
+
+def main():
+	with open(savePath + 'wgs/containers.index', 'rb') as f:
+		indexMatch = re.findall(re.compile(rb'\x00{4}\x10\x00{4}(.*?)$', re.MULTILINE|re.DOTALL), f.read())
+	
+	dirArray = {}
+
+	if indexMatch is not None:
+		indexContent = indexMatch[0]
+		
+		while len(indexContent) > 0:
+			numBytes = unpack('<i', indexContent[0:4])[0]
+			indexContent = indexContent[4:]
+
+			directoryName = unpack(f'<{str(numBytes * 2)}s', indexContent[0:numBytes * 2])[0].replace(b'\x00', b'').decode('ascii')
+			indexContent = indexContent[numBytes * 4 + 4:]
+
+			numBytes = unpack('<i', indexContent[0:4])[0]
+			indexContent = indexContent[9 + numBytes * 2:]
+
+			dirArray[directoryName] = retrieveMd5FileName(indexContent[0:16])
+			indexContent = indexContent[40:]
+
+		timestampStr = str(int(time()))
+		try: 
+			mkdir(savePath + timestampStr)
+		except FileExistsError:
+			print(f'The directory {timestampStr} already exists!')
+
+		for directory in dirArray:
+			try:
+				mkdir(f'{savePath}{timestampStr}/{directory}')
+			except FileExistsError:
+				print(f'The directory {savePath}{timestampStr}/{directory} already exists!')
+
+			with open(glob(f'{savePath}wgs/{dirArray[directory]}/container.*')[0], 'rb') as f:
+				containerContent = f.read()
+
+			numEntries = unpack('<i', containerContent[4:8])[0]
+			containerContent = containerContent[8:]
+			cpt = 1
+			while numEntries >= cpt:
+				entryContent = containerContent[0:0xa0]
+				containerContent = containerContent[0xa0:]
+				
+				#filenamePath = unpack(f'<{str(0x80)}s', entryContent[0:0x80])[0].replace(b'\x00', b'').replace(b'_S', b'/').decode('ascii')
+				filenamePath = unpack(f'<{str(0x80)}s', entryContent[0:0x80])[0].replace(b'\x00', b'').decode('ascii')
+				filenameMd5 = retrieveMd5FileName(entryContent[0x80:])
+
+				print(f'{filenamePath} {filenameMd5}')
+
+				with open(f'{savePath}wgs/{dirArray[directory]}/{filenameMd5}', 'rb') as f:
+					compressedFileContent = f.read()
+
+				arrayPath = filenamePath.split('_S')
+				intermediaryFolder = ''
+				if len(arrayPath) < 2:
+					filename = arrayPath[0]
+				else:
+					intermediaryFolder = arrayPath[0]
+					filename = arrayPath[1]
+					if not path.isdir(f'{savePath}{timestampStr}/{directory}/{intermediaryFolder}'):
+						mkdir(f'{savePath}{timestampStr}/{directory}/{intermediaryFolder}')
+
+				with open(f'{savePath}{timestampStr}/{directory}/{intermediaryFolder}/{filename}', 'wb') as f:
+					f.write(decompress(compressedFileContent[4:]))
+
+				cpt += 1
+
+if __name__ == "__main__":
+	main()
